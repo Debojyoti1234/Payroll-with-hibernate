02fbe742101a2f28f9c5d212d6f7e803c288c6c2
Debojyoti1234 <47375988+Debojyoti1234@users.noreply.github.com>
2019-02-19 18:38:16 +0530

    Add files via upload

---
+++ JPADemo/src/com/cg/project/beans/Employee.java
@@ -0,0 +1,63 @@
+package com.cg.project.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+private int employeeId;
+private String firstName;
+private String lastName;
+private  int salary;
+private String email;
+public Employee() {
+	super();
+}
+public Employee(int employeeId, String firstName, String lastName, int salary, String emailId) {
+	super();
+	this.employeeId = employeeId;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.salary = salary;
+	this.email = email;
+}
+public int getEmployeeId() {
+	return employeeId;
+}
+public void setEmployeeId(int employeeId) {
+	this.employeeId = employeeId;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public int getSalary() {
+	return salary;
+}
+public void setSalary(int salary) {
+	this.salary = salary;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+@Override
+public String toString() {
+	return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName + ", salary="
+			+ salary + ", email=" + email + "]";
+}
+
+
+
+}
